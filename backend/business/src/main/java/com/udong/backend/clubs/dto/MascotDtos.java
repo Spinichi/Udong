66e2e2f56555cc76ad97547ec70d64900d41ac34
bumy0409 <bumy0409@naver.com>
2025-09-16 15:50:57 +0900

    Revert "Merge branch 'S13P21A310-49' into 'master'"
    This reverts commit 43c9c09a0d24c61b1b178fa17f9ef5675c9176b9, reversing
    changes made to fefe8563e13874bbbe55df9dac15a0ec10ad72fc.

--- backend/business/src/main/java/com/udong/backend/clubs/dto/MascotDtos.java
+++
@@ -1,9 +0,0 @@
-package com.udong.backend.clubs.dto;
-
-
-import java.time.Instant;
-
-
-public class MascotDtos {
-    public record Res(Integer id, Integer clubId, String imageUrl, String promptMeta, Instant createdAt) {}
-}
--- backend/business/src/main/java/com/udong/backend/users/repository/UserAvailabilityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.udong.backend.users.repository;
-
-import com.udong.backend.users.entity.UserAvailability;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAvailabilityRepository extends JpaRepository<UserAvailability, Long> {}
